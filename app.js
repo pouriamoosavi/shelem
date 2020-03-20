d12397bad6a39f9ef1d32b63ce0a541f5014371e
pouria.moosavi <pouria.moosvi@gmail.com>
2020-03-20 18:20:19 +0330

    delete unnecessary imports and view config. add package-lock.json

--- app.js
+++ app.js
@@ -48,14 +48,14 @@ var logger = require('morgan');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
@@ -63,11 +63,11 @@ server.listen(3000);
 // WARNING: app.listen(80) will NOT work here!
 
 let allCards = [
-  {name: 'c1', img: 'ace_of_clubs'}, {name: 'c2', img: '2_of_clubs'}, {name: 'c3', img: '3_of_clubs'}, {name: 'c4', img: '4_of_clubs'}, {name: 'c5', img: '5_of_clubs'}, {name: 'c6', img: '6_of_clubs'}, {name: 'c7', img: '7_of_clubs'}, {name: 'c8', img: '8_of_clubs'}, {name: 'c9', img: '9_of_clubs'}, {name: 'c10', img: '10_of_clubs'}, {name: 'cj', img: 'jack_of_clubs2'}, {name: 'cq', img: 'queen_of_clubs2'}, {name: 'ck', img: 'king_of_clubs2'},
-  {name: 'd1', img: 'ace_of_diamonds'}, {name: 'd2', img: '2_of_diamonds'}, {name: 'd3', img: '3_of_diamonds'}, {name: 'd4', img: '4_of_diamonds'}, {name: 'd5', img: '5_of_diamonds'}, {name: 'd6', img: '6_of_diamonds'}, {name: 'd7', img: '7_of_diamonds'}, {name: 'd8', img: '8_of_diamonds'}, {name: 'd9', img: '9_of_diamonds'}, {name: 'd10', img: '10_of_diamonds'}, {name: 'dj', img: 'jack_of_diamonds2'}, {name: 'dq', img: 'queen_of_diamonds2'}, {name: 'dk', img: 'king_of_diamonds2'},
-  {name: 's1', img: 'ace_of_spades'}, {name: 's2', img: '2_of_spades'}, {name: 's3', img: '3_of_spades'}, {name: 's4', img: '4_of_spades'}, {name: 's5', img: '5_of_spades'}, {name: 's6', img: '6_of_spades'}, {name: 's7', img: '7_of_spades'}, {name: 's8', img: '8_of_spades'}, {name: 's9', img: '9_of_spades'}, {name: 's10', img: '10_of_spades'}, {name: 'sj', img: 'jack_of_spades2'}, {name: 'sq', img: 'queen_of_spades2'}, {name: 'sk', img: 'king_of_spades2'},
-  {name: 'h1', img: 'ace_of_hearts'}, {name: 'h2', img: '2_of_hearts'}, {name: 'h3', img: '3_of_hearts'}, {name: 'h4', img: '4_of_hearts'}, {name: 'h5', img: '5_of_hearts'}, {name: 'h6', img: '6_of_hearts'}, {name: 'h7', img: '7_of_hearts'}, {name: 'h8', img: '8_of_hearts'}, {name: 'h9', img: '9_of_hearts'}, {name: 'h10', img: '10_of_hearts'}, {name: 'hj', img: 'jack_of_hearts2'}, {name: 'hq', img: 'queen_of_hearts2'}, {name: 'hk', img: 'king_of_hearts2'},
-  {name: 'jb', img: 'black_joker'}, {name: 'jc', img: 'red_joker'},
+  {name: 'c1'}, {name: 'c2'}, {name: 'c3'}, {name: 'c4'}, {name: 'c5'}, {name: 'c6'}, {name: 'c7'}, {name: 'c8'}, {name: 'c9'}, {name: 'c10'}, {name: 'cj'}, {name: 'cq'}, {name: 'ck'},
+  {name: 'd1'}, {name: 'd2'}, {name: 'd3'}, {name: 'd4'}, {name: 'd5'}, {name: 'd6'}, {name: 'd7'}, {name: 'd8'}, {name: 'd9'}, {name: 'd10'}, {name: 'dj'}, {name: 'dq'}, {name: 'dk'},
+  {name: 's1'}, {name: 's2'}, {name: 's3'}, {name: 's4'}, {name: 's5'}, {name: 's6'}, {name: 's7'}, {name: 's8'}, {name: 's9'}, {name: 's10'}, {name: 'sj'}, {name: 'sq'}, {name: 'sk'},
+  {name: 'h1'}, {name: 'h2'}, {name: 'h3'}, {name: 'h4'}, {name: 'h5'}, {name: 'h6'}, {name: 'h7'}, {name: 'h8'}, {name: 'h9'}, {name: 'h10'}, {name: 'hj'}, {name: 'hq'}, {name: 'hk'},
+  {name: 'jb'}, {name: 'jc'},
 ]
 let thisGameCards = allCards;
 let db = {
@@ -82,9 +82,9 @@ let db = {
 }
 let numberOfPlayers = 4;
 
-app.get('/', function (req, res) {
-  res.sendFile('/index.html');
-});
+// app.get('/', function (req, res) {
+//   res.sendFile('/index.html');
+// });
 
 io.on('connection', async function (socket) {
   socket.on('join', async function(data) {
